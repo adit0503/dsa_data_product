cce6998c98980f486ade2623111b8f18f68290e6
adit0503 <adit0503@gmail.com>
2024-10-22 10:03:03 +0530

    strings LC

--- demo/src/test/java/com/leetcode/LC_Tests.java
+++ demo/src/test/java/com/leetcode/LC_Tests.java
@@ -7,6 +7,14 @@ import org.junit.jupiter.api.Test;
 public class LC_Tests {
 
     @Test
+    public void lc1768_test() {
+        assertEquals(LC1768.solution("null","null"), "nnuullll");
+        assertEquals(LC1768.solution("abc","pqr"), "apbqcr");
+        assertEquals(LC1768.solution("ab","pqrs"), "apbqrs");
+        assertEquals(LC1768.solution("abcd","pq"), "apbqcd");
+    }
+
+    @Test
     public void lc215_test() {
         assertEquals(LC215.solution(new int[]{3,2,1,5,6,4}, 2), 5);
         assertEquals(LC215.solution(new int[]{3,2,3,1,2,4,5,5,6}, 4), 4);
