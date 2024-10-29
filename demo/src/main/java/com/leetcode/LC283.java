eab594f2f5ca66ae0ef7766273d5833418d3a626
adit0503 <adit0503@gmail.com>
2024-10-29 07:19:04 +0530

    leetcode break

---
+++ demo/src/main/java/com/leetcode/LC283.java
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/move-zeroes/description/?envType=study-plan-v2&envId=leetcode-75
+package com.leetcode;
+
+
+public class LC283 {
+    
+    public static void solution(int[] nums) {
+        
+    }
+}
